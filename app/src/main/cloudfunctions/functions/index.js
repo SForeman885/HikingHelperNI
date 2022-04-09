3d5c377f15bac3cb7b57c4a714b43a36bbd4f790
Steven Foreman <steveforeman1@sky.com>
2022-04-09 23:39:34 +0100

    updating cloud function to work with hometown and db changes

--- app/src/main/cloudfunctions/functions/index.js
+++ app/src/main/cloudfunctions/functions/index.js
@@ -12,55 +12,49 @@ exports.calculateUserAverageSpeed = functions.firestore.document("Users/{userId}
     var totalSpeed = 0;
     var count = 0;
     return usersRef.doc(context.params.userId).collection("Logs").get()
-    .then(snapshot => {
-        snapshot.forEach(doc => {
-            if(doc.get("trailId") === undefined) {
+        .then(snapshot => {
+            snapshot.forEach(doc => {
                 var difficultyValue = getDifficultyValue(doc.get("difficulty"));
                 var elevationValue = doc.get("elevation");
                 var length = doc.get("length");
-            }
-            else {
-                var trailsRef = admin.firestore().collection("Trails");
-                var trail = trailsRef.doc(doc.get("trailId"));
-                var difficultyValue = getDifficultyValue(trail.get("difficulty"));
-                var elevationValue = trail.get("elevation");
-                var length = trail.get("length");
-            }
-            // elevation calculation based on Naismiths Rule (see documentation)
-            var elevationModifierValue = ((elevationValue*3.28)/1000)*30;
-            var timeTakenwithoutElevation = doc.get("timeTaken") - elevationModifierValue;
-            var finalTimeTaken = (timeTakenwithoutElevation / 60) * difficultyValue;
-            totalSpeed += (length/finalTimeTaken);
-            count ++;
+                // elevation calculation based on Naismiths Rule (see documentation)
+                var elevationModifierValue = ((elevationValue * 3.28) / 1000) * 30;
+                var timeTakenwithoutElevation = doc.get("timeTaken") - elevationModifierValue;
+                var finalTimeTaken = (timeTakenwithoutElevation / 60) * difficultyValue;
+                totalSpeed += (length / finalTimeTaken);
+                count++;
+            });
+            usersRef.doc(context.params.userId).get().then(userSnapshot => {
+                var hometown = userSnapshot.get("hometown");
+                if (count === 0) {
+                    console.log("no previous trails found");
+                    return usersRef.doc(context.params.userId).set({
+                        hometown: hometown
+                    });
+                }
+                else if (count >= 2) { // only have an averagespeed if more than 2 to ensure the value is useful
+                    var averageSpeed = totalSpeed / count;
+                    return usersRef.doc(context.params.userId).set({
+                        averageSpeed: averageSpeed,
+                        hometown: hometown
+                    });
+                }
+                else {
+                    return usersRef.doc(context.params.userId).set({
+                        hometown: hometown
+                    });
+                }
+            });
+        })
+        .catch(err => {
+            console.log('Error getting user logs', err);
         });
-        if(count === 0) {
-            console.log("no previous trails found");
-            return usersRef.doc(context.params.userId).set({
-                hometown: usersRef.get("hometown")
-            });
-        }
-        else if(count <= 2) { // only have an averagespeed if more than 2 to ensure the value is useful
-            var averageSpeed = totalSpeed/count;
-            return usersRef.doc(context.params.userId).set({
-                averageSpeed: averageSpeed,
-                hometown: usersRef.get("hometown")
-            });
-        }
-        else {
-            return usersRef.doc(context.params.userId).set({
-                hometown: usersRef.get("hometown")
-            });
-        }
-    })
-    .catch(err => {
-        console.log('Error getting user logs', err);
-    });
 });
 
 function getDifficultyValue(difficulty) {
     // if the log is an easy trail we want to 
     // increase the time taken to equal a more standard value
-    if(difficulty == "Easy") {
+    if (difficulty == "Easy") {
         return 1.25;
     }// for challenging we want to decrease for the same reason
     else if (difficulty == "Challenging") {
